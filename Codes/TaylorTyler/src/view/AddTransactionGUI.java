ced1a23189e414fa6207a2664c0fcabf6bfde1a3
Dezypher <lionkidzap@gmail.com>
2015-04-22 10:38:01 +0800

    AddTransactionGUI Update (again) :smile: :gun:
    - Subtotal and Total now updates when Clearing

--- Codes/TaylorTyler/src/view/AddTransactionGUI.java
+++ Codes/TaylorTyler/src/view/AddTransactionGUI.java
@@ -254,6 +254,13 @@ public class AddTransactionGUI extends JPanel implements ActionListener, TableMo
 		resetTables();
 		updateServiceTable();
 		updateProductTable();
+		
+		servicesSubtotal = 0;
+		productsSubtotal = 0;
+		totalPrice = 0;
+		servicesSubtotalLabel.setText("Products Subtotal: " + servicesSubtotal + " PHP");
+		productsSubtotalLabel.setText("Products Subtotal: " + productsSubtotal + " PHP");
+		totalLabel.setText("Total : " + totalPrice + " PHP");
 	}
 	
 	public void resetTables()
